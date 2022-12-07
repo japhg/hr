b0a8a9bee25179b9a57e7060bffe326e5a935893
Jpgomera19 <jpgomera19@gmail.com>
2022-12-08 01:51:45 +0800

    HR 1

---
+++ JobPortal/special_features/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '1f7c380654c45995f4c091df0de663d47e876610',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '1f7c380654c45995f4c091df0de663d47e876610',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.7',
+            'version' => '6.7.0.0',
+            'reference' => '80fc8686fcd070267b98dae0ec228d5d67d94310',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yidas/pagination' => array(
+            'pretty_version' => '1.0.7',
+            'version' => '1.0.7.0',
+            'reference' => 'd6b95bbab65e75b69517f755df3e23f93b4e0e78',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yidas/pagination',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
